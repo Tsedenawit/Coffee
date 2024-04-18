1f5cadabbf466efb8162a1f70d320f319db14f82
Tsedenawit <tsedenawit3@gmail.com>
2024-04-18 13:10:49 +0300

    sending

--- src/App.js
+++ src/App.js
@@ -18,25 +18,25 @@ function App() {
     <div className='bg-slate-400 mt-[300px] absolute ml-[120px]' >
       <h1 className=' text-2xl font-normal text-center'>Our collection</h1>
       <br></br>
-      <p className=''>Introducing our Coffee Collection, a selection of unique
+      <p className='text-center'>Introducing our Coffee Collection, a selection of unique
         coffees from different roast types and origins, expertly roasted
         in small batches and shipped fresh weekly.
       </p>
       <br></br>
       <div className='flex'>
-      <button className='bg-blue-100 max-w-25 text-center rounded'>All products</button>
-      <button>All products</button>
+      <button className='bg-blue-100 max-w-25 text-center rounded ml-10'>All products</button>
+      <button className='bg-blue-100 max-w-25 text-center rounded ml-10'>All products</button>
       </div>
-      <div className='grid grid-cols-3 gap-4 mt-10'>
+      <div className='grid grid-cols-3 gap-4 mt-10 ml-20'>
        {coffe.map((datas)=>
-      <div id={datas.id}>
+      <div key={datas.id}>
       {datas.popular && <div className ='bg-amber-400 rounded-full '><h3>Popular</h3></div>}
       <div>
-      <img src="../public/Images/bg-cafe.jpg" alt="not"/>
+      <img src={datas.image} alt="Coffee Shop Background Image" />
       </div>
       <div className='flex '>
       {datas.name}
-      <div className='bg-slate-300 max-w-20 text-center rounded-md content-end'>{datas.price}</div>
+      <div className='bg-slate-300 max-w-20 text-center rounded-md'>{datas.price}</div>
       </div></div>
       )} 
       </div>
