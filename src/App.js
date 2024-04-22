4a032928dc8d446efd6e89aaf214f77fcca11bc9
Tsedenawit <tsedenawit3@gmail.com>
2024-04-22 20:20:10 +0300

    send

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
   console.log(coffe )
   return (
     <div className='xl:bg-slate-400 mt-[300px] sm: mt-[90px] absolute grid bg-slate-400' >
-      <h1 className=' text-2xl font-normal text-center mt-5'>Our collection</h1>
+      <h1 className='xl:text-2xl font-normal text-center mt-5'>Our collection</h1>
       <br></br>
-      <p className='xl:text-center w-[400px] sm: w-[200px] '>Introducing our Coffee Collection, a selection of unique
+      <p className='xl:text-center w-[400px] items-center mt-0 mr-auto sm: w-[200px]'>Introducing our Coffee Collection, a selection of unique
         coffees from different roast types and origins, expertly roasted
         in small batches and shipped fresh weekly.
       </p>
@@ -38,13 +38,13 @@ function App() {
       
       <div className='flex '>
       {datas.name}
-      <div className='bg-slate-300 text-center w-[60px] rounded-md float-end'>{datas.price}</div>
+      <div className='bg-slate-300 text-center w-[60px] rounded-md mr-auto'>{datas.price}</div>
       </div>
       <div className='flex'>
       {datas.rating ? <img src='Images/Star_fill.svg'/>: <img src='Images/Star.svg'/> } 
       {datas.rating}
       <h2 className='text-grey-600'>({datas.votes} votes)</h2> 
-      {!datas.available && <h3 className='text-red-700 '>Sold out</h3>}
+      {!datas.available && <h3 className='text-red-700'>Sold out</h3>}
       </div>
       </div>
       )} 
